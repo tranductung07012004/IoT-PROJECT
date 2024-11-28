1ae9689b2fccf42e9fd6d1b51baa4ac10458a9b8
tung <tranductung07012004@gmail.com>
2024-11-28 10:58:05 +0700

    Init project

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const HOST = 'localhost';
+const expressHbs = require('express-handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine(
+  'hbs',
+  expressHbs.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+    extname: 'hbs',
+    defaultLayout: 'layout',
+  })
+);
+
+app.set('view engine', 'hbs');
+
+
+// Routes
+app.get('/', (req, res) => {  // Nếu truy cập đến đường dẫn root directory thì sẽ hiện ra login screen
+  res.render('login');
+})
+
+app.get('/register', (req, res) => {
+  res.render('register');
+})
+
+app.listen(PORT, HOST, () => {
+  console.log(`Listening on http://${HOST}:${PORT}`);
+});
